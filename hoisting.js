4d929fb08d5b8c5b7f37792630661d3e03c2f57a
adrianocruzweb <adrianocruzweb@hotmail.com>
2021-11-22 14:22:08 -0400

    feat: add hoisting of variables example

---
+++ hoisting.js
@@ -0,0 +1,20 @@
+function fn(){
+  console.log(text)
+
+  var text = 'texto de exemplo'
+
+  console.log(text)
+}
+
+fn()
+
+/* function fn(){
+  var text;
+  console.log(text)
+
+  text = 'texto de exemplo'
+
+  console.log(text)
+}
+
+fn() */
