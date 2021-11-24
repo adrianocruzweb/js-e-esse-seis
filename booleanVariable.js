9c7acc500cfe2f1426b45bb04db7992b01ef7f32
adrianocruzweb <adrianocruzweb@hotmail.com>
2021-11-24 13:43:56 -0400

    feat: add booleanVariable

---
+++ booleanVariable.js
@@ -0,0 +1,11 @@
+const isActive = true;
+
+
+const isAuthenticated = false;
+
+
+console.log('Tipo da variável: ', typeof isActive)
+console.log('\nValor 01: ', isActive)
+
+console.log('\nTipo da variável 02: ', typeof isAuthenticated)
+console.log('\n Valor variável 02: ', isAuthenticated)
